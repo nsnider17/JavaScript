1f59d60ba8526db285f45488f36527dd9163bb10
nsnider17 <nicksnider17@gmail.com>
2017-06-07 16:30:07 -0400

    added functions with parameters

--- 0.03_functions.js
+++ 0.03_functions.js
@@ -25,13 +25,71 @@ shoes("Crocs")
 function add(x, y){
 	console.log(x + y);
 }
-add(211394, 3502);
 
 
+//------------------Functions that return data
+
+//1
+function add(x, y){
+	return x+y;
+}
+var add = add(7, 3);
+console.log("1. "+ add);
 
 
+//2
+function sub(x, y){
+	return x-y;
+}
+var sub = sub(15, 5);
+console.log("2. "+ sub);
 
 
+//3
+function mul(x, y){
+	return x*y;
+}
+var mul = mul(5, 2);
+console.log("3. "+ mul);
+
+
+//4
+function div(x, y){
+	return x/y;
+}
+var div = div(5, 0.5);
+console.log("4. "+ div);
+
+
+//5
+function mod(x, y){
+	return x%y;
+}
+var mod = mod(10, 2);
+console.log("5. "+ mod);
+
+
+//Functions Challenges
+
+function stringAdd(string1, string2) {
+	return string1 + " " + string2;
+}
+var fullString = stringAdd("Hello", "World")
+console.log(fullString);
+
+
+function billTotal(water, electric, house, random) {
+	return water + electric + house + random;
+}
+var totalBillCost = billTotal(100, 125, 270, 85);
+console.log("You owe $" + totalBillCost + " this month.");
+
+function sodaTotal(cost, amount, tax) {
+	//return (amount*cost)+(amount*tax);
+	return ((cost*tax)*amount)+(cost*amount);
+}
+var totalSodaCost = sodaTotal(0.99, 100, 0.07);
+console.log("Your sodas will cost you $" + totalSodaCost + ".");
 
 
 
