9543ff53aed8c82c5dd553b27195f4f3368d08e8
nsnider17 <nicksnider17@gmail.com>
2017-06-08 14:53:45 -0400

    added more array work

--- 0.05_arrays.js
+++ 0.05_arrays.js
@@ -2,6 +2,100 @@
 
 //------------------------------Arrays
 
-var numbers = [0, 1, 2, 3, 4, 5];
+var fruit = ["Apples", "Oranges", "Bananas", "Strawberries"];
+console.log(fruit);
 
-console.log(numbers[2]);
+fruit.push("Rasberries");
+console.log(fruit);
+
+fruit.pop();
+console.log(fruit);
+
+//-----For In loop
+
+for (var f in fruit){
+	console.log(fruit[f])
+}
+
+
+// Create an array of numbers 1 - 10, then use a for in loop to print it to the console
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (var n in numbers){
+// 	console.log(numbers[n]);
+// }
+
+var blankArray = [];
+for (var num = 1; num < 11; num++){
+	console.log(num)
+	blankArray.push(num);
+	console.log(blankArray);
+}
+
+// for (var blank in blankArray){
+// 	console.log(blankArray[blank]);
+// }
+
+
+//-----Mixed Aarrays
+
+// var someThings = ["Nick", 7, [19, true], "iPhone"];
+// console.log(someThings[2][1]);
+
+var lists = [["Honda", "BMW", "Ford"], ["Webster, Dublin"], ["Mary", "Dave", "Nick"]];
+for (var list in lists) {
+	for (var x in lists[list]) {
+		console.log(lists[list][x])
+	}
+}
+
+var randomNumbers = [];
+
+for (var counter = 1; counter < 16; counter++){
+	var num = (Math.floor(Math.random()*50))
+	console.log(num);
+	randomNumbers.push(num)
+}
+
+var moreNumbers = [5, 19, 21, 40, 11, 25, 7, 14, 2, 1, 3];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
